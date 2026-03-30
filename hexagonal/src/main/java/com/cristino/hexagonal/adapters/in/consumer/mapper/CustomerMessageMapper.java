46e360a428270f1d39558baefc8fdbc233115947
Matheus Cristino <matheus.scristino@gmail.com>
2026-03-30 17:00:02 -0300

    Criaca do consumer do kafka

---
+++ hexagonal/src/main/java/com/cristino/hexagonal/adapters/in/consumer/mapper/CustomerMessageMapper.java
@@ -0,0 +1,12 @@
+package com.cristino.hexagonal.adapters.in.consumer.mapper;
+
+import com.cristino.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.cristino.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMessageMapper {
+    @Mapping(target = "address", ignore = true)
+    Customer toCustomer(CustomerMessage customerMessage);
+}
